6edd541f14e33cdb93bd0ab834354b4c97d163cc
kundan-2508 <kun123658@gmail.com>
2024-03-31 00:37:40 +0530

    Completed Inheritance

---
+++ Day9/inheritance.js
@@ -0,0 +1,12 @@
+// JS prototypes and Inheritance
+// The architecture of javascript is based on objects
+
+// A human get access to properties and methods(characteristics) of other human
+// In Javascript , this applies to objects
+// Everything in JS is an object
+
+let arr = [1,2,3];
+console.log(arr);
+
+let str = "hello world";
+console.log(str);
